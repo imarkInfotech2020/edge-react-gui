imarkInfotech2020/edge-react-gui

commit 1b3c184b809c252a760a30597da03e3acd8e2a6d

Hide fees if no amount is entered

src/components/scenes/SendScene2.tsx
@@ -528,7 +528,7 @@ const SendComponent = (props: Props) => {
   }
 
   const renderFees = () => {
-    if (spendInfo.spendTargets[0].publicAddress != null) {
+    if (spendInfo.spendTargets[0].publicAddress != null && spendInfo.spendTargets[0].nativeAmount != null) {
       const { noChangeMiningFee } = getSpecialCurrencyInfo(pluginId)
       let feeDisplayDenomination: EdgeDenomination
       let feeExchangeDenomination: EdgeDenomination
@@ -1022,7 +1022,7 @@ const SendComponent = (props: Props) => {
       pendingInsufficientFees.current = undefined
       try {
         setProcessingAmountChanged(true)
-        if (spendInfo.spendTargets[0].publicAddress == null) {
+        if (spendInfo.spendTargets[0].publicAddress == null || spendInfo.spendTargets[0].nativeAmount == null) {
           setEdgeTransaction(null)
           setSpendingLimitExceeded(false)
           setMaxSpendSetter(-1)

commit 06e2bb48bb33c5d9e05abbf1af44b7c61d3a633b

Fix max-spend logic
This was broken in 1b3c184b809c252a760a30597da03e3acd8e2a6d. The goal was to short-circuit the transaction calculation, but we don't want this in the max-spend case.

src/components/scenes/SendScene2.tsx
@@ -366,10 +366,6 @@ const SendComponent = (props: Props) => {
     needsScrollToEnd.current = true
   }
 
-  const handleSetMax = (index: number) => () => {
-    setMaxSpendSetter(index)
-  }
-
   const handleFeesChange = useHandler(() => {
     if (coreWallet == null) return
 
@@ -395,7 +391,7 @@ const SendComponent = (props: Props) => {
         feeTokenId={null}
         forceField={fieldChanged}
         onAmountsChanged={handleAmountsChanged(spendTarget)}
-        onMaxSet={handleSetMax(index)}
+        onMaxSet={() => setMaxSpendSetter(index)}
         onFeesChange={noChangeMiningFee ? undefined : handleFeesChange}
         wallet={coreWallet}
         tokenId={tokenId}
@@ -1022,7 +1018,7 @@ const SendComponent = (props: Props) => {
       pendingInsufficientFees.current = undefined
       try {
         setProcessingAmountChanged(true)
-        if (spendInfo.spendTargets[0].publicAddress == null || spendInfo.spendTargets[0].nativeAmount == null) {
+        if (spendInfo.spendTargets[0].publicAddress == null) {
           setEdgeTransaction(null)
           setSpendingLimitExceeded(false)
           setMaxSpendSetter(-1)